9d5d020893567616fc7a31440520764366a5a2f5
Kegan Maher <kegan.maher@smgov.net>
2016-04-13 10:49:57 -0700

    token to provide the "term path" from a TaxonomyField
    the term path is the full path of the first selected term.
    e.g. my-taxonomy/parent-term/selected-term
    
    another token is provided to remove the taxonomy from the term path.
    e.g. parent-term/selected-term

---
+++ Tokens/TaxonomyFieldTokens.cs
@@ -0,0 +1,56 @@
+﻿using Orchard.Localization;
+using Orchard.Taxonomies.Fields;
+using Orchard.Taxonomies.Services;
+using Orchard.Tokens;
+using System;
+using System.Linq;
+
+namespace CSM.Navigation.Tokens
+{
+    public class TaxonomyFieldTokens : ITokenProvider
+    {
+        public static string TermPath { get { return "TermPath"; } }
+        public static string TermPathNoTaxonomy { get { return "TermPath.NoTaxonomy"; } }
+
+        private readonly ITaxonomyService _taxonomyService;
+
+        public TaxonomyFieldTokens(ITaxonomyService taxonomyService)
+        {
+            _taxonomyService = taxonomyService;
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public void Describe(DescribeContext context)
+        {
+            // Usage:
+            // Content.Fields.PartWithTaxonomyField.TaxonomyFieldName.TermPath -> 'my-taxonomy/parent-category/child-category/grandchild-category/'
+            // Content.Fields.PartWithTaxonomyField.TaxonomyFieldName.TermPath.NoTaxonomy -> 'parent-category/child-category/grandchild-category/'
+
+            context.For("TaxonomyField", T("Taxonomy Field"), T("Tokens for Taxonomy Fields"))
+                   .Token(TermPath, T(TermPath), T("The complete term path associated with this field's selected term."))
+                   .Token(TermPathNoTaxonomy, T(TermPathNoTaxonomy), T("The term path associated with this field's selected term, minus the term's taxonomy name"));
+        }
+
+        public void Evaluate(EvaluateContext context)
+        {
+            context.For<TaxonomyField>("TaxonomyField")
+                   .Token(TermPath, field => Evaluate(field, false))
+                   .Token(TermPathNoTaxonomy, field => Evaluate(field, true));
+        }
+
+        private string Evaluate(TaxonomyField field, bool removeTaxonomy)
+        {
+            var term = field.Terms.FirstOrDefault();
+
+            if (term == null)
+                return null;
+
+            var taxonomy = _taxonomyService.GetTaxonomy(term.TaxonomyId);
+
+            string value = term.Slug.ToLower();
+            return removeTaxonomy ? value.Replace(taxonomy.Slug.ToLower() + "/", String.Empty) : value;
+        }
+    }
+}
