c62d3453df389a7445bbab4a80c8971cd970cbc0
Kegan Maher <kegan.maher@smgov.net>
2016-04-25 12:48:12 -0700

    define a part that configures a content type as a "landing page"
    our loose definition is an item that pulls in content from one or more other
    items

---
+++ Data/NavigationMigrations.cs
@@ -0,0 +1,30 @@
+﻿using CSM.Navigation.Models;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentPicker.Fields;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace CSM.Navigation.Data
+{
+    public class NavigationMigrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterPartDefinition(
+                typeof(LandingPagePart).Name,
+                part => part
+                    .Attachable(true)
+                    .WithDescription("Confgures the type as a landing page, with content coming from one or more other items.")
+                    .WithField("LandingContent", field => field
+                        .OfType(typeof(ContentPickerField).Name)
+                        .WithDisplayName("Landing Content")
+                        .WithSetting("ContentPickerFieldSettings.Required", "False")
+                        .WithSetting("ContentPickerFieldSettings.Multiple", "True")
+                        .WithSetting("ContentPickerFieldSettings.ShowContentTab", "True")
+                    )
+            );
+
+            return 1;
+        }
+    }
+}
---
+++ Models/LandingPagePart.cs
@@ -0,0 +1,8 @@
+﻿using Orchard.ContentManagement;
+
+namespace CSM.Navigation.Models
+{
+    public class LandingPagePart : ContentPart
+    {
+    }
+}
